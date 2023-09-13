b81ed7cc2bdd2f5577915b3046a22b91aef11274
Sergey Kudashev <kudashevs@gmail.com>
2023-09-13 12:31:35 +0300

    Style examples fix code style

--- example/index.php
+++ example/index.php
@@ -8,7 +8,7 @@ try {
     $calculate = Calculator($addition);
     echo $calculate(1, 2) . PHP_EOL; // 3
 } catch (\Exception $e) {
-    error_log('PHP Exception: ' . $e->getMessage() . ' in file ' . $e->getFile() . ' on line ' . $e->getLine(). '', 0);
+    error_log('PHP Exception: ' . $e->getMessage() . ' in file ' . $e->getFile() . ' on line ' . $e->getLine() . '', 0);
     echo $e->getMessage() . PHP_EOL;
 }
 
@@ -16,10 +16,10 @@ try {
     $calculate = Calculator($division);
     echo $calculate(1, 2) . PHP_EOL; // 0.5
 } catch (\DivisionByZeroError $e) {
-    error_log('PHP Exception: ' . $e->getMessage() . ' in file ' . $e->getFile() . ' on line ' . $e->getLine(). '', 0);
+    error_log('PHP Exception: ' . $e->getMessage() . ' in file ' . $e->getFile() . ' on line ' . $e->getLine() . '', 0);
     echo 'Division failed due to division by zero error!' . PHP_EOL;
 } catch (ClosureException $e) {
-    error_log('PHP Exception: ' . $e->getMessage() . ' in file ' . $e->getFile() . ' on line ' . $e->getLine(). '', 0);
+    error_log('PHP Exception: ' . $e->getMessage() . ' in file ' . $e->getFile() . ' on line ' . $e->getLine() . '', 0);
     echo 'Wrong Closure type passed to function!' . PHP_EOL;
 } catch (\ArgumentCountError $e) {
     error_log('PHP Exception: ' . $e->getMessage() . ' in file ' . $e->getFile() . ' on line ' . $e->getLine() . '', 0);
