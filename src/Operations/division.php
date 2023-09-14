a2a1adb78ce431bdb097b1770943468d29838dea
Sergey Kudashev <kudashevs@gmail.com>
2023-09-14 19:19:06 +0300

    Refactor division rename the division function parameter

--- src/Operations/division.php
+++ src/Operations/division.php
@@ -4,14 +4,14 @@ namespace CalculatorViaClosure\Operations;
 
 global $division;
 
-$division = function (...$args) {
-    if (in_array(0, $args, false)) {
+$division = function (...$arguments) {
+    if (in_array(0, $arguments, false)) {
         throw new \DivisionByZeroError('Cannot divide by zero.');
     }
 
-    $start = array_shift($args);
+    $start = array_shift($arguments);
 
-    return array_reduce($args, function ($acc, $v) {
+    return array_reduce($arguments, function ($acc, $v) {
         return $acc / $v;
     }, $start);
 };
