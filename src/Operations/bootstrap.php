8026857c276c6ad705c31196a5a6aec8b6632c00
Sergey Kudashev <kudashevs@gmail.com>
2023-09-14 23:43:37 +0300

    Update bootstrap move the generating full path responsibility to another function

--- src/Operations/bootstrap.php
+++ src/Operations/bootstrap.php
@@ -7,9 +7,7 @@ function retrieveOperationFiles(): array
 {
     $allFiles = scandir(__DIR__);
 
-    return array_map(function ($file) {
-        return __DIR__ . DIRECTORY_SEPARATOR . $file;
-    }, array_filter($allFiles, function ($file) {
+    return array_filter($allFiles, function ($file) {
         [$name, $extension] = explode('.', $file);
 
         return $extension === 'php'
@@ -20,15 +18,17 @@ function retrieveOperationFiles(): array
 
 function registerOperationsGlobally(array $files): void
 {
-    array_map(function ($file) {
-        $operationName = pathinfo($file)['filename'];
+    array_map(static function ($file) {
+        $operationName = basename($file, '.php');
         registerOperationGlobally($operationName, $file);
     }, $files);
 }
 
 function registerOperationGlobally(string $name, string $file)
 {
+    $fullPath = __DIR__ . DIRECTORY_SEPARATOR . $file;
+
     if (!array_key_exists($name, $GLOBALS)) {
-        $GLOBALS[$name] = require $file;
+        $GLOBALS[$name] = require $fullPath;
     }
 }
