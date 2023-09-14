4f7ded99757ba5df362c82c8b2643bb6d2aad19b
Sergey Kudashev <kudashevs@gmail.com>
2023-09-14 18:56:24 +0300

    Refactor addition rename the addition function parameter

--- src/Operations/addition.php
+++ src/Operations/addition.php
@@ -2,12 +2,12 @@
 
 namespace CalculatorViaClosure\Operations;
 
-return function (...$args) {
-    if (count($args) === 1) {
-        return $args[0];
+return function (...$arguments) {
+    if (count($arguments) === 1) {
+        return $arguments[0];
     }
 
-    return array_reduce($args, function ($acc, $v) {
+    return array_reduce($arguments, function ($acc, $v) {
         return $acc + $v;
     }, 0);
 };
