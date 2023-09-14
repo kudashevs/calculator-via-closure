df8437c3278c83885be3f2399f5c4407325579d6
Sergey Kudashev <kudashevs@gmail.com>
2023-09-14 21:22:34 +0300

    Update addition simplify the main calculation logic

--- src/Operations/addition.php
+++ src/Operations/addition.php
@@ -3,11 +3,5 @@
 namespace CalculatorViaClosure\Operations;
 
 return function (...$arguments) {
-    if (count($arguments) === 1) {
-        return $arguments[0];
-    }
-
-    return array_reduce($arguments, function ($acc, $v) {
-        return $acc + $v;
-    }, 0);
+    return array_sum($arguments);
 };
