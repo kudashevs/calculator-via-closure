4c5ef251c5b7b27bd7c635276bf99f67d3f785b9
Sergey Kudashev <kudashevs@gmail.com>
2023-09-15 09:19:15 +0300

    Style Calculator change the comments format in some functions

--- src/calculator.php
+++ src/calculator.php
@@ -10,10 +10,8 @@ function calculator(callable $operation)
 {
     checkValidCallable($operation);
 
-    /*
-     * This anonymous function is a Closure that encloses a provided callable $operation argument
-     * and uses it on the passed arguments as a math operation any time the Closure is invoked.
-     */
+    // This anonymous function is a Closure that encloses a provided callable $operation argument
+    // and uses it on the passed arguments as a math operation any time the Closure is invoked.
     return function (...$arguments) use ($operation) {
         checkValidArguments($arguments);
 
