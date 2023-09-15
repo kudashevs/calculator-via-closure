43cc04064c12b8228f5b4bdb9a6b0dfdbffa4aca
Sergey Kudashev <kudashevs@gmail.com>
2023-09-15 09:34:02 +0300

    Update examples improve the readability of some examples

--- example/index.php
+++ example/index.php
@@ -6,25 +6,23 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 try {
     $calculate = calculator($addition);
-    echo $calculate(1, 2) . PHP_EOL; // 3
-} catch (\Exception $e) {
-    error_log('PHP Exception: ' . $e->getMessage() . ' in file ' . $e->getFile() . ' on line ' . $e->getLine() . '', 0);
-    echo $e->getMessage() . PHP_EOL;
+    echo $calculate(1, 2) . PHP_EOL; // results in 3
+} catch (\Throwable $e) {
+    echo explainException($e);
 }
 
 try {
     $calculate = calculator($division);
-    echo $calculate(1, 2) . PHP_EOL; // 0.5
-} catch (\DivisionByZeroError $e) {
-    error_log('PHP Exception: ' . $e->getMessage() . ' in file ' . $e->getFile() . ' on line ' . $e->getLine() . '', 0);
-    echo 'Division failed due to division by zero error!' . PHP_EOL;
-} catch (\InvalidCallable $e) {
-    error_log('PHP Exception: ' . $e->getMessage() . ' in file ' . $e->getFile() . ' on line ' . $e->getLine() . '', 0);
-    echo 'A wrong callable was provided to the function.' . PHP_EOL;
-} catch (\ArgumentCountError $e) {
-    error_log('PHP Exception: ' . $e->getMessage() . ' in file ' . $e->getFile() . ' on line ' . $e->getLine() . '', 0);
-    echo 'Wrong arguments count passed to Closure!' . PHP_EOL;
-} catch (\TypeError $e) {
-    error_log('PHP Exception: ' . $e->getMessage() . ' in file ' . $e->getFile() . ' on line ' . $e->getLine() . '', 0);
-    echo 'Wrong arguments type passed to Closure!' . PHP_EOL;
+    echo $calculate(1, 2) . PHP_EOL; // results in 0.5
+} catch (\Throwable $e) {
+    echo explainException($e);
+}
+
+function explainException(\Throwable $exception): string
+{
+    return sprintf(
+        '%s: Something really went wrong with a message: %s' . PHP_EOL,
+        get_class($exception),
+        $exception->getMessage(),
+    );
 }
